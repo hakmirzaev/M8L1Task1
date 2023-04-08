46726cc08e7f9a183c4c1de124a6664b43c81a9f
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2023-04-08 15:34:27 +0500

    Initial Commit

---
+++ M8L1Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M8L1Task1
+//
+//  Created by Bekhruz Hakmirzaev on 08/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
